9c34020a2238b9ddedd876205d5a598e685ae043
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-09-08 00:35:12 +0530

    auth page done!

---
+++ app/routes/auth.tsx
@@ -0,0 +1,56 @@
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
+
+export const meta = () => ([
+    { title: 'JobsyAI | Auth' },
+    { name: 'description', content: 'Log into your account' },
+])
+
+const Auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    // whenever used tries to access a secrured route while not authenticated, they will be redirected back to auth with a next parameter
+    // that next parameter will be used to redirect them back to where they wanted to go after successful login
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    // this page will also have to handle redirection in case the user is already logged in
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next]) // recall function whenever the authenticated state changes
+
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
+                        ) : (
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className="auth-button" onClick={auth.signOut}>
+                                        <p>Log Out</p>
+                                    </button>
+                                ) : (
+                                    <button className="auth-button" onClick={auth.signIn}>
+                                        <p>Log In</p>
+                                    </button>
+                                )}
+                            </>
+                        )}
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default Auth
